c42a1112b8fef4f875bf28b529c3d171b432243e
kearnbread <mtkearney@wisc.edu>
2024-10-21 12:27:52 -0500

    correct HALT

--- project/demo1/verilog/fetch.v
+++ project/demo1/verilog/fetch.v
@@ -25,16 +25,16 @@ module fetch (clk, rst, PC_new, DUMP, PC_p2, instruction);
    ///////////////////////
    //PC_q stores current PC out and PC_p2 stores PC+2
    wire [15:0]PC_q;
-
+   wire [4:0]opcode = instruction[15:11];
 
    /////////////////////////////////
    // INSTANTIATE EXTERN. MODULES //
    /////////////////////////////////
    //DFFs hold value of PC
-   dff iPC[15:0](.q(PC_q), .d(PC_new), .clk(clk), .rst(rst));
+   dff iPC[15:0](.q(PC_q), .d(HALT ? PC_q : PC_new), .clk(clk), .rst(rst));
 
    //memory2c is Instruction Memory and outputs instruction pointed to by PC
-   memory2c iIM(.data_out(instruction), .data_in(16'h0000), .addr(PC_q), .enable(1'b1), .wr(1'b0), 
+   memory2c iIM(.data_out(instruction), .data_in(16'h0000), .addr(PC_q), .enable(~HALT), .wr(1'b0), 
                 .createdump(DUMP), .clk(clk), .rst(rst));
 
    ///////////
@@ -42,6 +42,17 @@ module fetch (clk, rst, PC_new, DUMP, PC_p2, instruction);
    ///////////
    //Keep PC_p2 as PC_q + 2
    cla_16b #(16) PCadder(.sum(PC_p2), .a(PC_q), .b(16'h0002), .c_in(1'b0));
+
+   reg HALT;
+   always @(opcode) begin
+      HALT = 1'b0;
+      case(opcode)
+         5'b00000: 
+            HALT = 1'b1;
+         default: 
+            HALT = 1'b0;
+      endcase
+   end
 
 
 
