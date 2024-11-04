eb93a36c97d2509b42c32cb583c283714d50f4f2
Henrywwg <henryjwg@gmail.com>
2024-11-03 22:46:41 -0600

    adding files from demo1

--- project/demo2/verilog/fetch.v
+++ project/demo2/verilog/fetch.v
@@ -3,11 +3,64 @@
   
    Filename        : fetch.v
    Description     : This is the module for the overall fetch stage of the processor.
+   Author          : Henry Wysong-Grass
+   Date            : 2024-10-09
+   Tested?         : NO
 */
 `default_nettype none
-module fetch (/* TODO: Add appropriate inputs/outputs for your fetch stage here*/);
-
-   // TODO: Your code here
+module fetch (clk, rst, PC_new, PC_p2, instruction, DUMP);
+   //Module Inputs
+   input wire clk;
+   input wire rst;
+   input wire [15:0]PC_new;
    
+   //Module Outputs
+   output wire [15:0]PC_p2;
+   output wire [15:0]instruction;
+   output wire DUMP;
+
+   ///////////////////////
+   // INTERNAL SIGNALS  //
+   ///////////////////////
+   //PC_q stores current PC out and PC_p2 stores PC+2
+   wire [15:0]PC_q;
+   wire [4:0]opcode;
+   reg HALT;
+
+   assign opcode = instruction[15:11];
+
+   /////////////////////////////////
+   // INSTANTIATE EXTERN. MODULES //
+   /////////////////////////////////
+
+   //DFFs hold value of PC
+   dff iPC[15:0](.q(PC_q), .d(HALT ? PC_q : PC_new), .clk(clk), .rst(rst));
+
+   //memory2c is Instruction Memory and outputs instruction pointed to by PC
+   memory2c iIM(.data_out(instruction), .data_in(16'h0000), .addr(PC_q), .enable(~HALT), .wr(1'b0), 
+                .createdump(1'b0), .clk(clk), .rst(rst));
+
+   ///////////
+   // LOGIC //
+   ///////////
+   //Keep PC_p2 as PC_q + 2
+   cla_16b #(16) PCadder(.sum(PC_p2), .a(PC_q), .b(16'h0002), .c_in(1'b0), .c_out());
+
+   ///////////////////////////////////////////////////////////////
+   // Create HALT Singal to stop processor and dump Data Memory //
+   ///////////////////////////////////////////////////////////////
+   always @(opcode) begin
+      HALT = 1'b0;
+      case(opcode)
+         5'b00000: 
+            HALT = 1'b1;
+         default: 
+            HALT = 1'b0;
+      endcase
+   end
+
+   assign DUMP = HALT;
+
+
 endmodule
 `default_nettype wire
--- project/demo2/verilog/memory.v
+++ project/demo2/verilog/memory.v
@@ -4,11 +4,33 @@
    Filename        : memory.v
    Description     : This module contains all components in the Memory stage of the 
                      processor.
+   Author          : Henry Wysong-Grass
+   Date            : 2024-10-09
+   Tested?         : NO
 */
 `default_nettype none
-module memory (/* TODO: Add appropriate inputs/outputs for your memory stage here*/);
-
-   // TODO: Your code here
+module memory (clk, rst, we, address, write_data, DUMP, read_data, en);
+   //Module Inputs
+   input wire clk;
+   input wire rst;
+   input wire we;
+   input wire en;
+   input wire [15:0]address;
+   input wire [15:0]write_data;
+   input wire DUMP;
    
+   //Module Outputs
+   output wire [15:0]read_data;
+
+
+   /////////////////////////////////
+   // INSTANTIATE EXTERN. MODULES //
+   /////////////////////////////////
+
+   //memory2c is Memory and outputs values pointed to be address
+   memory2c iIM(.data_out(read_data), .data_in(write_data), .addr(address), .enable(en), 
+                .wr(we), .createdump(DUMP), .clk(clk), .rst(rst));
+
+
 endmodule
 `default_nettype wire
